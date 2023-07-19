80c25c37196beba955061af418d71269c922fdb1
Ali <ali.arioglu06@gmail.com>
2023-07-19 14:08:47 +0300

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      Landing Page (Unprotected)
+      <div></div>
+    </div>
+  );
+};
+
+export default LandingPage;
